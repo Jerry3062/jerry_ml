969ef484915e34dd8b039a928ad366d0410b49ac
Jerry3062 <qwe258sj@163.com>
2018-04-10 17:00:17 +0800

    from thinkpad

---
+++ jerry_matplotlib/plot_demo.py
@@ -0,0 +1,12 @@
+import numpy as np
+import seaborn
+import matplotlib.pyplot as plt
+
+x = np.linspace(0, 2 * np.pi, 50)
+print(x)
+plt.subplot(2, 1, 1)
+plt.plot(x, np.sin(x), 'r', x, np.sin(2 * x))
+plt.show()
+plt.subplot(2, 1, 1)
+plt.plot(x, np.cos(x), 'g')
+plt.show()
---
+++ jerry_matplotlib/scatter_demo.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.linspace(0,np.pi,20)
+y = np.sin(x)
+plt.scatter(x,y)
+plt.show()
